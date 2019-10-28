cbada2fa6564292f933e74ec23b333c27a54ad17
ChoiJeongHwan <jeff8014@gmail.com>
2019-10-29 04:29:35 +0900

    expanded

--- raspberry/save.py
+++ raspberry/save.py
@@ -1,36 +1,62 @@
+import time
+import Adafruit_DHT
+import threading
+import RPi.GPIO as GPIO
 from flask import Flask, render_template
 from flaskext.mysql import MySQL
-import threading
-app = Flask(__name__)
+
+app = Flask(__name__)       # for flask
+sensor = Adafruit_DHT.DHT11 # DHT11 init
+pin_temp = 4                
+pin_led = 17
+GPIO.cleanup()
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin_led, GPIO.OUT)
 
 mysql = MySQL()
 #MYSQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'choi'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'chOicha0'
 app.config['MYSQL_DATABASE_DB'] = 'iotProject'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_HOST'] = '3.133.139.175'
 mysql.init_app(app)
 conn = mysql.connect()
 cursor = conn.cursor()
 
 def saveData():
-    cursor.callproc('store_temp_val', (20.5, 60.5))
-    data = cursor.fetchall()
-    if len(data) is 0:
-        conn.commit()
-        print ('data stroed successfully!')
+    try:
+        while True:
+            # 1. Get Data from sensor
+            h, t = Adafruit_DHT.read_retry(sensor, pin_temp)
+            if h is not None and t is not None :
+                print("Temperature = {0:0.1f}*C Humidity = {1:0.1f}%".format(t, h))
+            else :
+                print('Read error')
+            # 2. Store Data in mysql
+            cursor.callproc('store_temp_val', (t, h, 'MY_SENSOR'))
+            data = cursor.fetchall()
+            # 3. check for error
+            if len(data) is 0:
+                conn.commit()
+                print ('data stroed successfully!')
+            else :
+                print ('error occur' + data[0])
+            # 4. sleep for 5 minute
+            time.sleep(5 * 60)  
+    except KeyboardInterrupt:
+        return
+
+
+@app.route('/led/<status>', methods=['GET'])
+def changeLED(status):
+    if status == 'ON':
+        GPIO.output(pin_led, True)
+        time.sleep(0.1)
     else :
-        print ('error occur' + data[0])
-
-
-@app.route('/')
-def index():
-    cursor.execute('select * from iotProject.tbl_sensor_temp order by sensor_id DESC limit 5;')
-    data = cursor.fetchall()
-    return render_template('index.html', results = data)
-
+        GPIO.output(pin_led, False)
+        time.sleep(0.1)
 
 if __name__ == '__main__':
     t1 = threading.Thread(target=saveData)
     t1.start()
-    app.run()
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- webServer/app.py
+++ webServer/app.py
@@ -1,9 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+from flaskext.mysql import MySQL
+import threading
+import requests
 app = Flask(__name__)
+
+mysql = MySQL()
+#MYSQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'choi'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'chOicha0'
+app.config['MYSQL_DATABASE_DB'] = 'iotProject'
+app.config['MYSQL_DATABASE_HOST'] = '3.133.139.175'
+mysql.init_app(app)
+def sql_select():
+    conn = mysql.connect()    
+    cursor = conn.cursor()
+    cursor.execute('select * from iotProject.tbl_sensor_temp order by sensor_id DESC limit 5;')
+    data = cursor.fetchall()
+    conn.close()
+    return data
+
+@app.route('/led', methods=['GET'])
+def led():
+    a = request.args.get('input', 0, type=int)
+    if a == 0:        
+        requests.get('http://127.0.0.1:5000/led/ON')
+    else :
+        requests.get('http://127.0.0.1:5000/led/OFF')
+    return 'SUCCESS'
+
+@app.route('/getData', methods=['GET'])
+def getData():
+    data = sql_select()
+    return jsonify(data)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080, debug=True)
