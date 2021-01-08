80dbcb2eb5acc76967c913fe285deb2570e29de0
jyjun1015 <jyjun1015@naver.com>
2021-01-09 01:44:14 +0900

    0109_nosql

--- webServer/app.py
+++ webServer/app.py
@@ -6,11 +6,12 @@ app = Flask(__name__)
 
 mysql = MySQL()
 #MYSQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'choi'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'chOicha0'
-app.config['MYSQL_DATABASE_DB'] = 'iotProject'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'pildong'
+app.config['MYSQL_DATABASE_DB'] = 'example'
 app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
-api_service_key = 'rTgkWoe%2FDfnKVsvwwiN8fT%2B6Sw7aiOMWaWH7YGczxqvnTHINnvTMv6PEHqA72S0ra3lAmKDtBhBcGxFisdD1ig%3D%3D'
+
+# api_service_key = 'rTgkWoe%2FDfnKVsvwwiN8fT%2B6Sw7aiOMWaWH7YGczxqvnTHINnvTMv6PEHqA72S0ra3lAmKDtBhBcGxFisdD1ig%3D%3D'
 
 mysql.init_app(app)
 def sql_select():
@@ -21,25 +22,25 @@ def sql_select():
     conn.close()
     return data
 
-@app.route('/led', methods=['GET'])
-def led():
-    a = request.args.get('input', 0, type=int)
-    if a == 0:
-        requests.get('http://210.94.181.91:8080/led/ON')
-    else :
-        requests.get('http://210.94.181.91:8080/led/OFF')
-    return 'SUCCESS'
+# @app.route('/led', methods=['GET'])
+# def led():
+#     a = request.args.get('input', 0, type=int)
+#     if a == 0:
+#         requests.get('http://210.94.181.91:8080/led/ON')
+#     else :
+#         requests.get('http://210.94.181.91:8080/led/OFF')
+#     return 'SUCCESS'
 
-@app.route('/NodeMCU', methods=['GET', 'POST'])
-def test():
-    print ("NODEMCU CONNECTED!!!!!!!")
-    value = request.data.decode('utf-8')
-    print ("MESSAGE : " + value)
-    return "Connection Successed!"
+# @app.route('/NodeMCU', methods=['GET', 'POST'])
+# def test():
+#     print ("NODEMCU CONNECTED!!!!!!!")
+#     value = request.data.decode('utf-8')
+#     print ("MESSAGE : " + value)
+#     return "Connection Successed!"
 
-@app.route('/getAirCondition', methods=['GET'])
-def getAirCondition():
-    return requests.get('http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?sidoName=%EC%84%9C%EC%9A%B8&pageNo=1&numOfRows=10&ServiceKey=' + api_service_key + '&ver=1.3&_returnType=json').json()
+# @app.route('/getAirCondition', methods=['GET'])
+# def getAirCondition():
+#     return requests.get('http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?sidoName=%EC%84%9C%EC%9A%B8&pageNo=1&numOfRows=10&ServiceKey=' + api_service_key + '&ver=1.3&_returnType=json').json()
 
 
 @app.route('/getData', methods=['GET'])
